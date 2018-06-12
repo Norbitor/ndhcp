a957960a908a858e0972f99c61e65fd5d6b2140f
Norbert Langner <norbitor@gmail.com>
2018-06-12 21:53:57 +0200

    Simple listening

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import server
 
 def main():
-    server.DHCPServer()
+    s = server.DHCPServer()
+    try:
+        s.listen()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        s.process = False
+        s.shutdown()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import configparser
 import misc
 import sys
+from socket import *
 
 class DHCPServer:
     def __init__(self):
@@ -12,6 +13,33 @@ class DHCPServer:
             self.log.critical("Configuration file not found or its invalid. " \
                               "Please provide correct config.ini file.")
             sys.exit(-1)
+        self.process = True
+        self._setup()
+
+    def _setup(self):
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+        self.socket.setsockopt(SOL_IP, IP_MULTICAST_TTL, 20)
+        self.socket.setsockopt(SOL_IP, IP_MULTICAST_LOOP, 1)
+        self.socket.setsockopt(SOL_IP, IP_MULTICAST_IF, inet_aton(gethostbyname(gethostname())))
+
+    def listen(self):
+        self.socket.bind(('', 67))
+        while self.process:
+            data, _ = self.socket.recvfrom(1024)
+            self.log.info("Received broadcast message on port 67")
+        self.socket.close()
+
+        print(data)
+
+    def shutdown(self):
+        self.log.info("Gracefully stopping listening...")
+        try:
+            self.socket.shutdown(SHUT_RD)
+        except OSError:
+            pass
+        self.log.info("Server stopped.")
 
 class DHCPResolver:
     pass
